cb90c5bfa954e56dfdf766789e6e3d2ace1177d7
Anandhakrishnan <kalaichelvamanandhakrishnan@gmail.com>
2023-02-08 12:49:02 +0530

    First Commit on 08-Feb-22

--- guess_work/lib/main.dart
+++ guess_work/lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import 'constants.dart';
+import 'app_bar_main.dart';
 
 void main() {
-  runApp(guess());
-}
-
-class guess extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          // ignore: prefer_const_constructors
-          title: Text(
-            "Guess Work",
-            style: const TextStyle(color: appbarfontcolor),
-          ),
-          centerTitle: true,
-          backgroundColor: primarycolor,
-        ),
-      ),
-    );
-  }
+  runApp(const GuessAppBar());
 }
